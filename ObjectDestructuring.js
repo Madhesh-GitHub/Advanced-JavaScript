a03dbc0e0865e3829dac279594d89f681b384c4d
Madhesh K <madheshk7239087@gmail.com>
2025-12-20 10:41:11 +0530

    delete the old js files

--- ObjectDestructuring.js
+++
@@ -1,9 +0,0 @@
-const dreamHoliday = {
-    destination: 'Google, America',
-    activity: 'spending my time in the company',
-    accommodation: 'luxury ranch',
-    companion: 'Sundar Pichai'
-}
-
-const {destination, activity, accommodation, companion} = dreamHoliday;
-console.log(`My dream destination is ${destination}, there I do ${activity}, I stay in ${accommodation}, there my companion will be Mr. ${companion}`)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Advanced-JavaScript
-This repo contains advanced JavaScript code snippets which i create and push here after learning it from internet. If anybody who want to learn from this, then feel free to use this generously.  
